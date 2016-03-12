ce450120235449dbe4c157ec246fd6806077c490
Tim Churchard <tim.churchard@iotic-labs.com>
2016-03-12 13:07:24 +0000

    sucker.py move sqlite3 file ops to single thread.

--- sucker.py
+++ sucker.py
@@ -6,9 +6,10 @@ import sqlite3
 
 from sys import exit
 from time import sleep
-from threading import Lock
-from functools import partial
+from queue import Queue, Empty
 from base64 import b64encode
+from functools import partial
+from threading import Thread, Event
 
 from IoticAgent import IOT
 
@@ -57,40 +58,53 @@ def table_exists(conn, tablename):
     return ret
 
 
-def data_cb(lock, data):
+def data_cb(queue, data):
     if not (data['mime'] is None and isinstance(data['data'], dict)):
         print("Error: data must be automatically decoded to dict.  Give up.")
         return
     for key in data['data']:
         if isinstance(data['data'][key], bool):
             data['data'][key] = int(data['data'][key])
-    with lock:
-        tablename = PREFIX + data['pid']
-        sdb = sqlite3.connect(DBFILE)
-        if not table_exists(sdb, tablename):
-            sql = data_to_create(tablename, data['data'])
-            print(sql)
-            cur = sdb.cursor()
-            cur.execute(sql)
-            cur.close()
-        sql = data_to_insert(tablename, data['data'])
-        print(sql)
-        cur = sdb.cursor()
-        cur.execute(sql)
-        cur.close()
-        sdb.commit()
-        sdb.close()
+    tablename = PREFIX + data['pid']
+    queue.put(data_to_create(tablename, data['data']))
+    queue.put(data_to_insert(tablename, data['data']))
+
+
+def sqlite_thread(stop, queue):
+    sdb = sqlite3.connect(DBFILE)
+    while True:
+        try:
+            sql = queue.get(timeout=2)
+        except Empty:
+            if stop.is_set():
+                break
+            continue
+        if sql.startswith("CREATE TABLE "):
+            tablename = sql[13:]
+            tablename = tablename[:tablename.find(' ')]
+            if table_exists(sdb, tablename):
+                continue
+        print("sqlite_thread: ", sql)
+        try:
+            sdb.execute(sql)
+            sdb.commit()
+        except Exception as exc:
+            print("Failed with %s" % exc)
+    sdb.close()
 
 
 if __name__ == '__main__':
     #
-    data_lock = Lock()
+    stop = Event()
+    queue = Queue()
+    thread = Thread(target=sqlite_thread, args=(stop, queue,))
+    thread.start()
     #
     client = None
     try:
         client = IOT.Client()
-        client.register_catchall_feeddata(partial(data_cb, data_lock))
-        client.register_catchall_controlreq(partial(data_cb, data_lock))
+        client.register_catchall_feeddata(partial(data_cb, queue))
+        client.register_catchall_controlreq(partial(data_cb, queue))
         client.start()
     except Exception as exc:
         print("Failed to start.  Give up.")
@@ -102,5 +116,7 @@ if __name__ == '__main__':
             sleep(60)
         except KeyboardInterrupt:
             break
+    print("Stopping...")
     client.stop()
-
+    stop.set()
+    thread.join()
